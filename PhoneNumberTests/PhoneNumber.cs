5cd523166eb2523ddd40e60a11bb26165757d3e2
Chris Lucian <Chris.Lucian@HunterIndustries.com>
2023-08-17 13:23:21 -0700

    Converted the interview to cSharp

---
+++ PhoneNumberTests/PhoneNumber.cs
@@ -0,0 +1,27 @@
+public class PhoneNumber
+{
+
+    public PhoneNumber(string rawText)
+    {
+    }
+
+    public string GetOriginalText()
+    {
+        return "";
+    }
+
+    public string GetStrippedNumber()
+    {
+        return "";
+    }
+
+    public string GetValueAsNorthAmerican()
+    {
+        return "";
+    }
+
+    public string GetValueAsInternational()
+    {
+        return "";
+    }
+}
